14ef5921bdbddc27492efb61fa9037c0ce95b958
Yosuke Ishikawa <y@ishkawa.org>
2016-01-01 23:43:18 +0900

    Add multiple tokens support

--- main.go
+++ main.go
@@ -3,31 +3,52 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
-	token := os.Getenv("TOKEN")
-	if token == "" {
-		log.Fatalln("TOKEN is not found in environment varables")
-	}
+	conns := []*RTMConn{}
+	for _, token := range strings.Split(os.Getenv("TOKEN"), ",") {
+		conn, err := OpenRTMConn(token)
+		if err != nil {
+			log.Fatalln("Could not open RTM connection:", err)
+		}
 
-	conn, err := OpenRTMConn(token)
-	if err != nil {
-		log.Fatalln("Could not open RTM connection:", err)
+		conns = append(conns, conn)
 	}
 
 	go func() {
 		for {
 			time.Sleep(time.Minute)
-			conn.Ping()
+
+			for _, conn := range conns {
+				conn.Ping()
+			}
 		}
 	}()
 
-	for {
-		msg, err := conn.ReceiveMsg()
-		if err == nil {
-			log.Println(msg)
-		}
+	ch := make(chan string)
+
+	for _, conn := range conns {
+		go func(conn *RTMConn) {
+			for {
+				time.Sleep(time.Minute)
+				conn.Ping()
+			}
+		}(conn)
+
+		go func(conn *RTMConn) {
+			for {
+				msg, err := conn.ReceiveMsg()
+				if err == nil {
+					ch <- msg
+				}
+			}
+		}(conn)
+	}
+
+	for msg := range ch {
+		log.Println(msg)
 	}
 }
