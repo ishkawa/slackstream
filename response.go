833fafc4ca0e8d5d89dc8e3132ee9f395f0dd7fe
Yosuke Ishikawa <y@ishkawa.org>
2016-01-01 15:56:23 +0900

    Separate response.go from main.go

---
+++ response.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/url"
+	"strings"
+)
+
+// Channel is a JSON object that represents a channel.
+type Channel struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// User is a JSON object that represents a user.
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Event is a JSON object that represents a event.
+type Event struct {
+	ID        string `json:"id"`
+	ChannelID string `json:"channel"`
+	UserID    string `json:"user"`
+	Type      string `json:"type"`
+	Subtype   string `json:"subtype"`
+	Text      string `json:"text"`
+	Ts        string `json:"ts"`
+}
+
+// RTMInfo represents a root JSON response of GET /api/rtm.start.
+type RTMInfo struct {
+	RawURL   string    `json:"url"`
+	Channels []Channel `json:"channels"`
+	Users    []User    `json:"users"`
+}
+
+// URL returns url.URL from RTMInfo.RawURL.
+func (info *RTMInfo) URL() (*url.URL, error) {
+	URL, err := url.Parse(info.RawURL)
+	if err != nil {
+		return nil, err
+	}
+
+	comps := strings.Split(URL.Host, ":")
+	if err != nil {
+		return nil, err
+	}
+
+	// Append port if it is missing because net/websocket needs port for wss.
+	if len(comps) == 1 {
+		URL.Host += ":443"
+	}
+
+	return URL, nil
+}
